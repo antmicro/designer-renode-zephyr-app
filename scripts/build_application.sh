e3b79b6d1b7fea6a1ed3fb846c55f601245f4e03
Marcin Witkowski <mwitkowski@antmicro.com>
2024-08-28 18:17:43 +0200

    Include shell_module source code

--- scripts/build_application.sh
+++ scripts/build_application.sh
@@ -5,5 +5,5 @@ ROOT_DIR="$(cd ${THIS_DIR}/.. && pwd)"
 
 cd zephyrproject/zephyr
 
-west build --pristine -b nrf52840dk_nrf52840 samples/subsys/shell/shell_module
+west build --pristine -b nrf52840dk_nrf52840 ${ROOT_DIR}/source
 cp build/zephyr/zephyr.elf ${ROOT_DIR}/artifacts/software.elf
