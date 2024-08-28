1824c55bcb481d9f4a045fef8633f1339a0f979e
Marcin Witkowski <mwitkowski@antmicro.com>
2024-08-28 16:48:46 +0200

    Add GitHub Action for building software

---
+++ scripts/build_application.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+THIS_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+ROOT_DIR="$(cd ${THIS_DIR}/.. && pwd)"
+
+cd zephyrproject/zephyr
+
+west build --pristine -b nrf52840dk_nrf52840 samples/subsys/shell/shell_module
+cp build/zephyr/zephyr.elf ${ROOT_DIR}/artifacts/software.elf
