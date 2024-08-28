f2fa69474ee6ea9143e26e88e29bcd5d99b60eba
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2024-08-28 18:36:28 +0200

    Try to use Zephyr build action

--- scripts/build_application.sh
+++ scripts/build_application.sh
@@ -3,7 +3,8 @@
 THIS_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 ROOT_DIR="$(cd ${THIS_DIR}/.. && pwd)"
 
-cd zephyrproject/zephyr
+mkdir -p artifacts
+cd zephyr
 
 west build --pristine -b nrf52840dk_nrf52840 samples/subsys/shell/shell_module
 cp build/zephyr/zephyr.elf ${ROOT_DIR}/artifacts/software.elf
