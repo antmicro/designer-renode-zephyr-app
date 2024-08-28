712b1f4b3fe47c9d97c4ef0d0bdc3311b34a5a37
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2024-08-28 19:36:44 +0200

    Fix application path

--- scripts/build_application.sh
+++ scripts/build_application.sh
@@ -2,5 +2,5 @@
 
 mkdir -p artifacts
 
-west build --pristine -b nrf52840dk_nrf52840 source
+west build --pristine -b nrf52840dk_nrf52840 source/app
 cp build/zephyr/zephyr.elf artifacts/software.elf
