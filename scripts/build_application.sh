7a3528172948a8010bea7758be5bc57050691c61
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2024-08-28 18:55:03 +0200

    Simplify build

--- scripts/build_application.sh
+++ scripts/build_application.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-THIS_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
-ROOT_DIR="$(cd ${THIS_DIR}/.. && pwd)"
-
 mkdir -p artifacts
-cd zephyr
 
-west build --pristine -b nrf52840dk_nrf52840 ${ROOT_DIR}/source
-cp build/zephyr/zephyr.elf ${ROOT_DIR}/artifacts/software.elf
+west build --pristine -b nrf52840dk_nrf52840 source
+cp build/zephyr/zephyr.elf artifacts/software.elf
